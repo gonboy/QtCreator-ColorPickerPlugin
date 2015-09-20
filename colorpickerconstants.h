fd252bd02062063955cc74f30b2cb4ec062a36df
AxelAF <axel.airofarulla@gmail.com>
2015-09-20 20:47:25 +0200

    Add more color regex

--- colorpickerconstants.h
+++ colorpickerconstants.h
@@ -1,26 +1,48 @@
-#ifndef COLORPICKERCONSTANTS
-#define COLORPICKERCONSTANTS
+#ifndef COLORPICKERCONSTANTS_H
+#define COLORPICKERCONSTANTS_H
 
-#include <QString>
+#include <string>
 
 namespace ColorPicker {
 namespace Internal {
 namespace Constants {
 
-// Regexp
-QString REGEX_HSL = QStringLiteral("hsl\\s*?\\(\\s*([0-9]|[1-9][0-9]|[1|2][0-9][0-9]|3[0-5][0-9]|360)\\s*?,\\s*?([0-9]|[1-9][0-9]|100)\\%?\\s*?,\\s*?([0-9]|[1-9][0-9]|100)\\%?\\s*\\)");
-QString REGEX_HSLA = QStringLiteral("hsla\\s*?\\(\\s*([0-9]|[1-9][0-9]|[1|2][0-9][0-9]|3[0-5][0-9]|360)\\s*?,\\s*?([0-9]|[1-9][0-9]|100)\\%?\\s*?,\\s*?([0-9]|[1-9][0-9]|100)\\%?\\s*?,\\s*?(0|1|1.0|0*\\.\\d+)\\s*?\\)");
-QString REGEX_HSV = QStringLiteral("hsv\\s*?\\(\\s*([0-9]|[1-9][0-9]|[1|2][0-9][0-9]|3[0-5][0-9]|360)\\s*?,\\s*?([0-9]|[1-9][0-9]|100)\\%?\\s*?,\\s*?([0-9]|[1-9][0-9]|100)\\%?\\s*\\)");
-QString REGEX_HSVA = QStringLiteral("hsva\\s*?\\(\\s*([0-9]|[1-9][0-9]|[1|2][0-9][0-9]|3[0-5][0-9]|360)\\s*?,\\s*?([0-9]|[1-9][0-9]|100)\\%?\\s*?,\\s*?([0-9]|[1-9][0-9]|100)\\%?\\s*?,\\s*?(0|1|1.0|0*\\.\\d+)\\s*?\\)");
-QString REGEX_VEC3 = QStringLiteral("vec3\\s*?\\(\\s*?([0]?\\.[0-9]*|1\\.0|1|0)[f]?\\s*?\\,\\s*?([0]?\\.[0-9]*|1\\.0|1|0)[f]?\\s*?\\,\\s*?([0]?\\.[0-9]*|1\\.0|1|0)[f]?\\s*?\\)");
-QString REGEX_VEC4 = QStringLiteral("vec4\\s*?\\(\\s*?([0]?\\.[0-9]*|1\\.0|1|0)[f]?\\s*?\\,\\s*?([0]?\\.[0-9]*|1\\.0|1|0)[f]?\\s*?\\,\\s*?([0]?\\.[0-9]*|1\\.0|1|0)[f]?\\s*?\\,\\s*?([0]?\\.[0-9]*|1\\.0|1|0)[f]?\\s*?\\)");
-QString REGEX_RGB = QStringLiteral("rgb\\s*?\\(\\s*?([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\s*?,\\s*?([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\s*?,\\s*?([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\s*?\\)");
-QString REGEX_RGBA = QStringLiteral("rgba\\s*?\\(\\s*?([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][<0-9]|25[0-5])\\s*?,\\s*?([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\s*?,\\s*?([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\s*?,\\s*?(0|1|1.0|0*\\.\\d+)\\s*?\\)");
-QString REGEX_HEXCOLOR = QStringLiteral("(\\#[a-f0-9]{6}|\\#[a-f0-9]{3})");
+
+////////////////////////// Regex parts //////////////////////////
+
+const std::string _RXPART_RGBA_INT = "\\s*?([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
+const std::string _RXPART_HUE = "\\s*([0-9]|[1-9][0-9]|[1|2][0-9][0-9]|3[0-5][0-9]|360)";
+const std::string _RXPART_SATURATION_LUMINANCE_VALUE = "\\s*?,\\s*?([0-9]|[1-9][0-9]|100)";
+const std::string _RXPART_FLOAT_LITERAL = "\\s*?([0]?\\.[0-9]*|1\\.0|1|0)[f]";
+const std::string _RXPART_FLOAT_VALUE = "\\s*?(0|1|1.0|0*\\.\\d+)";
+
+
+////////////////////////// Regex //////////////////////////
+
+// Base colors
+const std::string REGEX_HSL = "hsl\\s*?\\(" + _RXPART_HUE + _RXPART_SATURATION_LUMINANCE_VALUE + "\\%?" + _RXPART_SATURATION_LUMINANCE_VALUE + "\\%?\\s*\\)";
+const std::string REGEX_HSLA = "hsla\\s*?\\(" + _RXPART_HUE + _RXPART_SATURATION_LUMINANCE_VALUE + _RXPART_SATURATION_LUMINANCE_VALUE + "\\%?" + _RXPART_SATURATION_LUMINANCE_VALUE + "\\%?\\s*?," + _RXPART_FLOAT_VALUE + "\\s*?\\)";
+const std::string REGEX_HSV = "hsv\\s*?\\(" + _RXPART_HUE + _RXPART_SATURATION_LUMINANCE_VALUE + "\\%?" + _RXPART_SATURATION_LUMINANCE_VALUE + "\\%?\\s*\\)";
+const std::string REGEX_HSVA = "hsva\\s*?\\(" + _RXPART_HUE + _RXPART_SATURATION_LUMINANCE_VALUE + "\\%?" + _RXPART_SATURATION_LUMINANCE_VALUE + "\\%?\\s*?," + _RXPART_FLOAT_VALUE + "\\s*?\\)";
+const std::string REGEX_VEC3 = "vec3\\s*?\\(" + _RXPART_FLOAT_LITERAL + "?\\s*?\\," + _RXPART_FLOAT_LITERAL + "?\\s*?\\," + _RXPART_FLOAT_LITERAL + "?\\s*?\\)";
+const std::string REGEX_VEC4 = "vec4\\s*?\\(" + _RXPART_FLOAT_LITERAL + "?\\s*?\\," + _RXPART_FLOAT_LITERAL + "?\\s*?\\," + _RXPART_FLOAT_LITERAL + "?\\s*?\\," + _RXPART_FLOAT_LITERAL + "?\\s*?\\)";
+const std::string REGEX_RGB = "rgb\\s*?\\(" + _RXPART_RGBA_INT + "\\s*?," + _RXPART_RGBA_INT + "\\s*?," + _RXPART_RGBA_INT + "\\s*?\\)";
+const std::string REGEX_RGBA = "rgba\\s*?\\(" + _RXPART_RGBA_INT + "\\s*?," + _RXPART_RGBA_INT + "\\s*?," + _RXPART_RGBA_INT + "\\s*?," + _RXPART_FLOAT_VALUE + "\\s*?\\)";
+const std::string REGEX_HEXCOLOR = "(\\#[a-f0-9]{6}|\\#[a-f0-9]{3})";
+
+// Qt colors
+const std::string REGEX_QCOLOR_INLINE_CTOR_RGB = "QColor\\s*?\\(" + _RXPART_RGBA_INT + "\\s*?," + _RXPART_RGBA_INT + "\\s*?," + _RXPART_RGBA_INT + "\\s*?(|(," + _RXPART_RGBA_INT + "))\\s*?\\)";
+const std::string REGEX_QCOLOR_STATIC_CTOR_RGB = "";
+const std::string REGEX_QCOLOR_FROM_HSL = "";
+const std::string REGEX_QCOLOR_FROM_HSLA = "";
+const std::string REGEX_QCOLOR_FROM_HSV = "";
+const std::string REGEX_QCOLOR_FROM_HSVA = "";
+const std::string REGEX_QCOLOR_FROM_RGB = "";
+const std::string REGEX_QCOLOR_FROM_RGBA = "";
 
 } // namespace Constants
-} // namespace ColorPicker
 } // namespace Internal
+} // namespace ColorPicker
 
-#endif // COLORPICKERCONSTANTS
+#endif // COLORPICKERCONSTANTS_H
 
